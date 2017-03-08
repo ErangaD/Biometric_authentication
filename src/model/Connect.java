b793104cc83d8f0cf17279df52133d5ff0ca3c04
erangad <erangadulshan.14@cse.mrt.ac.lk>
2017-03-08 23:04:43 +0530

    create login functions

--- src/model/Connect.java
+++ src/model/Connect.java
@@ -6,6 +6,8 @@
 package model;
 
 import java.sql.*;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 
 
 /**
@@ -14,7 +16,31 @@ import java.sql.*;
  */
 public class Connect {
     static Connection conn=null;
-    
+    static Statement stmt = null;
+    static {
+        try{
+            Class.forName("org.sqlite.JDBC");
+            conn=DriverManager.getConnection("jdbc:sqlite:biometrixAuth.sqlite");
+            stmt = conn.createStatement();
+            String sql1="CREATE TABLE IF NOT EXISTS \"Hand\" (\"user_id\" INTEGER NOT NULL , \"thumb_length\" VARCHAR, \"thumb_width\" VARCHAR, \"index_length\" VARCHAR, \"index_width\" VARCHAR, \"middle_length\" VARCHAR, \"middle_width\" VARCHAR, \"ring_length\" VARCHAR, "
+                    + "\"ring_width\" VARCHAR, \"baby_length\" VARCHAR, \"baby_width\" VARCHAR)";
+            stmt.executeUpdate(sql1);
+            String sql2="CREATE TABLE IF NOT EXISTS \"User\" (\"id\" INTEGER PRIMARY KEY  AUTOINCREMENT  NOT NULL ,"
+                    + " \"name\" CHAR NOT NULL , \"user_id\" CHAR NOT NULL )";
+            stmt.executeUpdate(sql2);
+            stmt.close();
+        }
+        catch(Exception e){
+            System.out.println(e);
+        }finally{
+            try {
+                if(conn!=null)
+                    conn.close();
+            } catch (SQLException ex) {
+                Logger.getLogger(Connect.class.getName()).log(Level.SEVERE, null, ex);
+            }
+        }
+    }
     public static Connection connectDb(){
         try{
             Class.forName("org.sqlite.JDBC");
@@ -25,6 +51,7 @@ public class Connect {
         catch(Exception e){
             System.out.println(e);
         }
+        
         return null;
     }
 }
